8b407379f1c610f7fb296f74177e48e332c7603e
Damen Birtola <dbirtola@ucsc.edu>
2018-04-24 22:35:36 -0600

    Initial Fucking Commit

---
+++ Source/FortKnight/UselessClassToDelete.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UselessClassToDelete.h"
+
+UselessClassToDelete::UselessClassToDelete()
+{
+}
+
+UselessClassToDelete::~UselessClassToDelete()
+{
+}
---
+++ Source/FortKnight/UselessClassToDelete.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+
+/**
+ * 
+ */
+class FORTKNIGHT_API UselessClassToDelete
+{
+public:
+	UselessClassToDelete();
+	~UselessClassToDelete();
+};
